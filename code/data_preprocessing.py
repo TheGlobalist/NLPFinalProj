6f69e8fae0a3652cbdecb4c3cfdcecc1ae572ab4
Gianmarco <g.forcella94@gmail.com>
2020-02-17 00:03:46 +0100

    Maybe end of preprocessing. Starting to dig in training + fixed a small problem with the model's skeleton

--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -1,10 +1,13 @@
 from typing import List, Set, Dict
-from bs4 import BeautifulSoup
+from collections import defaultdict
 import string
 from nltk.corpus import wordnet as wn
 from lxml import etree
+import tensorflow as tf
+import os
 from io import StringIO, BytesIO
 import time
+tf.compat.v1.enable_eager_execution()
 
 def load_dataset(filepath_train: str, filepath_label_train: str) -> List[str]:
     """
@@ -13,35 +16,63 @@ def load_dataset(filepath_train: str, filepath_label_train: str) -> List[str]:
     :param filepath_label_train: the path of the file that holds the labels
     :return the loaded dataset
     """
-    print("entered")
-    print("Loading with lxml")
-    t = time.process_time()
+    is_it_multilingual = any(lang in os.path.basename(filepath_label_train) for lang in ["it","es","de","fr"])
+    path_for_eventual_existing_label_data = '../dataset/parsed/' + ('eval_dataset/parsed' if is_it_multilingual else '') +os.path.basename(filepath_label_train[0:-3]+'casted.txt')
+    path_for_eventual_existing_train_data = '../dataset/parsed/' + ('eval_dataset/parsed' if is_it_multilingual else '') +os.path.basename(filepath_train[0:-3]+'casted.txt')
+    train_data = None
+    train_labels = None
+    if os.path.exists(path_for_eventual_existing_train_data):
+        train_data = __reload_data_from_disk(path_for_eventual_existing_train_data)
+    if os.path.exists(path_for_eventual_existing_label_data):
+        train_labels = __reload_data_from_disk(path_for_eventual_existing_label_data, data='label')
+        return train_data, train_labels
+
     with open(filepath_train, "rb") as infile:
         tree_train = etree.fromstring(infile.read())
-    print("Loading done in ", (time.process_time() - t))
-    print("Creating the sentences...")
-    t = time.process_time()
+
     sentences = __load_train_sentences(tree_train)
-    print("Sentences created in ", (time.process_time() - t))
-    print("Cleaning...")
-    t = time.process_time()
     train_data = [__clean_phrase(sentence) for sentence in sentences]
-    print("Clean done in ", (time.process_time() - t) )
-    print("Loading ", filepath_label_train)
+
+    with open(path_for_eventual_existing_train_data, 'a') as train:
+        for frase in train_data:
+            train.write("#"+frase+"#")
+
     with open(filepath_label_train, encoding="utf-8") as infilez:
         contents_label = infilez.read().splitlines()
-    #Assuming that the ids are unique
-    #Also, cleaning empty strings
-    print("Cleaning...")
+
     contents_label = [label for label in contents_label if label != "" or label is not None]
-    print("Creating dict")
     pool_of_labels = {frase.split(" ")[0] : frase.split(" ")[1] for frase in contents_label}
-    print("Creating labels...")
-    train_labels = __create_labeled_sentences(pool_of_labels,tree_train)
-    return train_data
+    tree_train_sentences = [sentence for sentence in tree_train.xpath("/*/*/*")]
+    train_labels = __create_labeled_sentences(pool_of_labels,tree_train_sentences, path_for_eventual_existing_label_data)
+    return train_data, train_labels
+
+def __reload_data_from_disk(filepath: str, data: str ='train'):
+    """
+    Reload the previously parsed files from disk
+    :param filepath: the path of the file to be loaded
+    :param data: whether the file is for train or other purposes
+    :return: a List (if data == 'train') or a List of List (if data != 'train') containing the data of the loaded file
+    """
+    with open(filepath, 'r') as writer:
+        content = writer.read()
+    toReturn = []
+    if data == 'train':
+        toReturn = [word for word in content[1:-1].split('#')]
+        return toReturn
+    for word in content[1:-1].split('#'):
+        if not word or word == '':
+            continue
+        tmp = [frase for frase in word.split('~')]
+        toReturn.append(tmp)
+    return toReturn
 
 
-def __load_train_sentences(tree : etree._Element ):
+def __load_train_sentences(tree : etree._Element ) -> List:
+    """
+    Parse the lxml tree object to get the train sentences
+    :param tree: the parsed .xml objet
+    :return: the list containing all the train sentences
+    """
     toReturn = []
     sentences_xml_elements = tree.xpath("/*/*/*")
     for sentence_xml in sentences_xml_elements:
@@ -50,53 +81,102 @@ def __load_train_sentences(tree : etree._Element ):
         toReturn.append(phraseToBuild)
     return toReturn
 
-def __create_labeled_sentences(pool_of_labels: dict, tree : etree._Element ) -> None:
+def __create_labeled_sentences(pool_of_labels: dict, tree_train_sentences : List, filepath: str) -> List[List]:
+    """
+    Creates the labels for the model
+    :param pool_of_labels: the gold key file parsed to a dict
+    :param tree: the parsed .xml file
+    :param filepath: where to save the output of this function
+    :return: a List of List which contains all the labeled sentences
+    """
     toReturn = []
-    sentences_xml_elements = tree.xpath("/*/*/*")
-    for sentence in sentences_xml_elements:
+    wn_domains_sentence = ""
+    bn_domains_sentence = ""
+    lex_domains_sentence = ""
+    for word in tree_train_sentences:
+        name = word.xpath("name()")
+        if name is not None or name is not "":
+            if name == 'wf':
+                wn_domains_sentence += ' ' + word.text
+                bn_domains_sentence += ' ' + word.text
+                lex_domains_sentence += ' ' + word.text
+                continue
+            if name == 'instance':
+                id = word.get('id')
+                sense_key = pool_of_labels[id]
+                wordnet_synset = wn.lemma_from_key(sense_key).synset()
+                synset_id = "wn:" + str(wordnet_synset.offset()).zfill(8) + wordnet_synset.pos()
+                wn_domains_sentence += ' ' + put_correct_id(word.text,synset_id,mode='wn_dom')
+                bn_domains_sentence += ' ' + put_correct_id(word.text,synset_id)
+                lex_domains_sentence += ' ' + put_correct_id(word.text,synset_id,mode='lex')
+                continue
+        else:
+            continue
+        toReturn.append([wn_domains_sentence.strip(),bn_domains_sentence.strip(),lex_domains_sentence.strip()])
         wn_domains_sentence = ""
         bn_domains_sentence = ""
         lex_domains_sentence = ""
-        children = sentence.getchildren()
-        for word in sentence:
-            name = word.xpath("name()")
-            if name is not None or name is not "":
-                if name == 'wf':
-                    wn_domains_sentence += ' ' + word.text
-                    bn_domains_sentence += ' ' + word.text
-                    lex_domains_sentence += ' ' + word.text
-                    continue
-                if name == 'instance':
-                    id = word.get('id')
-                    sense_key = pool_of_labels[id]
-                    print("getting wordnet_synset")
-                    t = time.process_time()
-                    wordnet_synset = wn.lemma_from_key(sense_key).synset()
-                    print("Done in ", (time.process_time() - t))
-                    synset_id = "wn:" + str(wordnet_synset.offset()).zfill(8) + wordnet_synset.pos()
-                    wn_domains_sentence += ' ' + synset_id
+    with open(filepath, 'a') as writer:
+        for matrix in toReturn:
+            writer.write('#' + matrix[0].strip() + '~' + matrix[1].strip() + '~' + matrix[2].strip() + '#')
+    return toReturn
 
-                    continue
-            else:
-                continue
 
-        #getting all things to predict...
-        instances = sentence.find_all('instance')
-
-        print("ciao")
-
-def put_correct_id(synset_id: str, mode: str='bn') -> str:
+def put_correct_id(word:str, synset_id: str, mode: str='bn') -> str:
+    """
+    Finds the correct synset id for the given id
+    :param word: the word that the system is evaluating right now
+    :param synset_id: the id associated with the current :word
+    :param mode: the id that I have to look for. Possible options: BabelNet (bn), WordNet Domains (wn_dom), LexNames (lex)
+    :return: the found id if exists, else the word that the system is looking in this exact moment
+    """
     if mode == 'wn_dom':
         try:
-            pass
-            #Cerca nella mappa
+            synset_bn = [key for key, value in bn2wn.items() if synset_id in value][0]
+            synset_wn_dom = bn2wndomains[synset_bn][0]
+            return synset_wn_dom
         except:
-            pass
-            #Non l'ho trovato. Metti factotum
+            return "factotum"
+    if mode == 'bn':
+        for key, value in bn2wn.items():
+            if synset_id in value:
+                return key
     else:
-        pass
-        #semplicemnete, cercalo normalmente
+        try:
+            synset_bn = [key for key, value in bn2wn.items() if synset_id in value][0]
+            synset_lex = bn2lex[synset_bn][0]
+            return synset_lex
+        except IndexError:
+            import traceback
+            traceback.print_exc()
+            return word
+    return word
 
+
+def calculate_train_output_size(train_data: List, max_size_of_vocab: int = 30000, min_count: int = 3):
+    """
+    Calculates the output size of the train data
+    :param train_data: the list that will represent the train data
+    :param max_size_of_vocab: max size of the vocab. Usually goes for 30k
+    :param min_count: min count of the vocab. Usually is 3
+    :return: an integer with the size of the output_size unit
+    """
+    pass
+
+
+
+def find_count_of_words(data, max_size_of_vocab: int, min_count: int, mode: str = "word") -> Dict:
+    """
+    Calculate the number of words
+    :param data: the data where to count the words. Can either be a list or a list of list.
+    :param max_size_of_vocab: max size of the vocab. Usually goes for 30k
+    :param min_count: min count of the vocab. Usually is 3
+    :param mode: whether I need to count the #words, or #bn ids or #lex ids or #wndmn ids
+    :return: the count inside a dict ordered like word -> count
+    """
+    toReturn = None
+    if mode == 'bn':
+        pass
 
 
 def __clean_phrase(sentence: str) -> str:
@@ -114,16 +194,25 @@ def __clean_phrase(sentence: str) -> str:
 
 
 def __load_synset_mapping(filepath: str) -> Dict:
+    """
+    Loads the synset mapping from the system
+    :param filepath: the path of the file to load
+    :return: a dict representation of the loaded file
+    """
+    dizionario = defaultdict(list)
     with open(filepath) as file:
         content = file.read()
-    dictionary = {mapping.split()[0]:mapping.split()[1] for mapping in content.split("\n") if mapping}
-    return dictionary
+    for arr in content.split("\n"):
+        for key, value in zip(arr.split()[::-1], arr.split()[1::-1]):
+            dizionario[key] += [value] if type(value) is str else value
+    return dizionario
 
-wndomains2bn = __load_synset_mapping('../resources/babelnet2wordnet.tsv')
-wn2bn = __load_synset_mapping('../resources/babelnet2wndomains.tsv')
-lex2bn = __load_synset_mapping('../resources/babelnet2wlexnames.tsv')
+
+bn2wn = __load_synset_mapping('../resources/babelnet2wordnet.tsv')
+bn2wndomains = __load_synset_mapping('../resources/babelnet2wndomains.tsv')
+bn2lex = __load_synset_mapping('../resources/babelnet2lexnames.tsv')
 
 
 if __name__ == "__main__":
-    lista = load_dataset("../dataset/SemCor/semcor.data.xml", "../dataset/SemCor/semcor.gold.key.txt")
-    print(lista[17:100])
+    train,label = load_dataset("../dataset/SemCor/semcor.data.xml", "../dataset/SemCor/semcor.gold.key.txt")
+    print(train[17:100])
--- code/model.py
+++ code/model.py
@@ -1,80 +1,79 @@
 import tensorflow as tf
-from tensorflow import keras as keras
 from typing import List
 import tensorflow_hub as hub
 from tokenizer import FullTokenizer
 import os
-
-
-def convert_sentence_to_features(sentence, tokenizer, max_seq_len):
-    tokens = ['[CLS]']
-    tokens.extend(tokenizer.tokenize(sentence))
-    if len(tokens) > max_seq_len - 1:
-        tokens = tokens[:max_seq_len - 1]
-    tokens.append('[SEP]')
-
-    segment_ids = [0] * len(tokens)
-    input_ids = tokenizer.convert_tokens_to_ids(tokens)
-    input_mask = [1] * len(input_ids)
-
-    # Zero Mask till seq_length
-    zero_mask = [0] * (max_seq_len - len(tokens))
-    input_ids.extend(zero_mask)
-    input_mask.extend(zero_mask)
-    segment_ids.extend(zero_mask)
-
-    return input_ids, input_mask, segment_ids
-
-
-def convert_sentences_to_features(sentences, tokenizer, max_seq_len=20):
-    all_input_ids = []
-    all_input_mask = []
-    all_segment_ids = []
-
-    for sentence in sentences:
-        input_ids, input_mask, segment_ids = convert_sentence_to_features(sentence, tokenizer, max_seq_len)
-        all_input_ids.append(input_ids)
-        all_input_mask.append(input_mask)
-        all_segment_ids.append(segment_ids)
-
-    return all_input_ids, all_input_mask, all_segment_ids
-
-def create_tokenizer(vocab_file, do_lower_case=False):
-    return FullTokenizer(vocab_file=vocab_file, do_lower_case=do_lower_case)
-
+import time
+from sklearn.model_selection import train_test_split
+from data_preprocessing import load_dataset
+from tokenizer import FullTokenizer
 
 
 class WSD:
-    def __init__(self,hidden_size: int = 256, input_length: int = None,dropout: float = 0.0,recurrent_dropout: float = 0.0,
-                 learning_rate: float = None,vocab_size: int = None,outputs_size: List = None):
+    def __init__(self,hidden_size: int = 256, dropout: float = 0.0, recurrent_dropout: float = 0.0,
+                 learning_rate: float = 0.01, outputs_size: List = None):
         input_word_ids = tf.keras.layers.Input(shape=(128,), dtype=tf.int32, name="input_word_ids")
         input_mask = tf.keras.layers.Input(shape=(128,), dtype=tf.int32, name="input_mask")
         segment_ids = tf.keras.layers.Input(shape=(128,), dtype=tf.int32, name="segment_ids")
         #BERt = BERtLayer()([input_word_ids, input_mask, segment_ids])
         bert = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/1", trainable=True)
-        LSTM = keras.layers.Bidirectional(
-        keras.layers.LSTM(
-            units=hidden_size,
-            dropout=dropout,
-            recurrent_dropout=recurrent_dropout,
-            return_sequences=True,
-            return_state=True,
-        ))(bert[1])
+        pooled_output, sequence_output = bert([input_word_ids, input_mask, segment_ids])
+        self.vocab_file = bert.resolved_object.vocab_file.asset_path.numpy()
+        self.do_lower_case = bert.resolved_object.do_lower_case.numpy()
+        LSTM = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(
+                units=hidden_size,
+                dropout=dropout,
+                recurrent_dropout=recurrent_dropout,
+                return_sequences=True,
+                return_state=True,
+            )
+        )(sequence_output)
         lstm_attention = self.attention_layer(LSTM)
         #We need to perform multitask learning, so we need 3 outputs...
-        babelnet = keras.layers.Dense(outputs_size[0], activation="softmax", name="bn")(lstm_attention)
-        domain = keras.layers.Dense(outputs_size[1], activation="softmax", name="dom")(lstm_attention)
-        lexicus = keras.layers.Dense(outputs_size[2], activation="softmax", name="lex")(lstm_attention)
+        babelnet = tf.keras.layers.Dense(outputs_size[0], activation="softmax", name="bn")(lstm_attention)
+        domain = tf.keras.layers.Dense(outputs_size[1], activation="softmax", name="dom")(lstm_attention)
+        lexicus = tf.keras.layers.Dense(outputs_size[2], activation="softmax", name="lex")(lstm_attention)
         #Usage of AdamOptimizer in order to have a better train results
-        optimizer = keras.optimizers.Adam(lr=learning_rate)
-        sess = keras.backend.get_session()
+        optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+        sess = tf.keras.backend.get_session()
         init = tf.global_variables_initializer()
         sess.run(init)
-        self.model = keras.models.Model(inputs=[input_word_ids,input_mask, segment_ids], outputs=[babelnet,domain,lexicus])
+        self.model = tf.keras.models.Model(inputs=[input_word_ids,input_mask, segment_ids], outputs=[babelnet,domain,lexicus])
         self.model.compile(
             loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["acc"]
         )
         self.model.summary()
+
+    def train(self):
+        """
+        WIP...
+        :return:
+        """
+        early_stopper = tf.keras.callbacks.EarlyStopping(
+            monitor="val_loss", patience=4, mode="min", verbose=1, restore_best_weights=True
+        )
+
+        if not os.path.exists("../saved_model"):
+            os.makedirs("../saved_model")
+        cp_path = "../saved_model/model_{epoch:02d}_{val_loss:.2f}.h5"
+        cp = tf.keras.callbacks.ModelCheckpoint(
+            cp_path,
+            monitor="val_loss",
+            verbose=1,
+            save_best_only=True,
+            save_weights_only=True,
+            mode="min",
+        )
+
+        train_data, label_data = load_dataset("../dataset/SemCor/semcor.data.xml","../dataset/SemCor/semcor.gold.key.txt")
+
+        train_data_es, label_data_es = load_dataset("../dataset/eval_dataset/semeval2015.es.data.xml","../dataset/eval_dataset/semeval2015.es.gold.key.txt")
+
+        tokenizatore = FullTokenizer(self.vocab_file,do_lower_case=self.do_lower_case)
+        test = tokenizatore.tokenize(train_data[0])
+        print("ciao")
+
 
 
     def attention_layer(self, lstm):
@@ -84,15 +83,16 @@ class WSD:
         :param lstm: The LSTM that will be used in the task
         :return: The LSTM that was previously given in input with the enhancement of the Attention Layer
         """
-        hidden_state = keras.layers.Concatenate()([lstm[1], lstm[3]]) #Layer that concatenates a list of inputs.
-        hidden_state = keras.layers.RepeatVector(keras.backend.shape(lstm[0])[1])(hidden_state)
-        u = keras.layers.Dense(1, activation="tanh")(hidden_state)
-        a = keras.layers.Activation("softmax")(u)
-        context_vector = keras.layers.Lambda(lambda x: keras.backend.sum(x[0] * x[1], axis=1))([lstm[0], a])
-        return keras.layers.Multiply()([lstm[0], context_vector])
+        hidden_state = tf.keras.layers.Concatenate()([lstm[1], lstm[3]]) #Layer that concatenates a list of inputs.
+        hidden_state = tf.keras.layers.RepeatVector(tf.keras.backend.shape(lstm[0])[1])(hidden_state)
+        u = tf.keras.layers.Dense(1, activation="tanh")(hidden_state)
+        a = tf.keras.layers.Activation("softmax")(u)
+        context_vector = tf.keras.layers.Lambda(lambda x: tf.keras.backend.sum(x[0] * x[1], axis=1))([lstm[0], a])
+        return tf.keras.layers.Multiply()([lstm[0], context_vector])
 
 
 if __name__ == "__main__":
     testing = WSD()
+    testing.train()
     print("ciao")
 
